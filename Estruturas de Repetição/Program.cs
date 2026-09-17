334fb1367914907e807471d9fc2be8ef26e10b68
Nicolas Kronemberger <kronemxd@gmail.com>
2026-09-17 20:01:29 -0300

    Add files via upload

---
+++ Estruturas de Repetição/Program.cs
@@ -0,0 +1,98 @@
+﻿// Estruturas de Repetição (repetem)
+
+// while (enquanto)
+
+//Console.WriteLine("Digite a senha : ");
+//string senha = Console.ReadLine();
+
+//while (senha != "vini")
+//{
+//    Console.WriteLine("Senha incorreta. Tente novamente.!");
+//    Console.WriteLine("Digite a senha : ");
+//    senha = Console.ReadLine();
+//}
+
+// do/while  
+
+// for    
+
+
+//int contador = 1;
+
+//while (contador <= 5)
+//{
+//    Console.WriteLine($"Repetição número: {contador}.");
+//    // contador = contador + 1;
+
+//    contador++; // mesma coisa do codigo de cima.
+//}
+//Console.WriteLine("FIM!");
+
+
+
+// Use um laço While para exibir apenas numeros pares 
+// de 1 ate 20 . (dica utilize if e operador %)
+
+//int numero = 1; 
+
+//while (numero <= 20) // se repete ate o numero ser maior que 20
+//{
+//    if (numero % 2 == 0) 
+//    { 
+//        Console.WriteLine(numero);
+//    }
+//    numero++;  // adiciona mais 1 
+//}
+
+
+// faça um laço while que o contador inicie em 30 e termine em 0 
+
+//int contador = 30;
+//while (contador >= 0)  // enquanto contador for maior ou igual a 0.
+//{
+//    Console.WriteLine($"A Contagem esta em {contador}");
+//    contador--;
+//}
+
+
+
+// DP whoçe -> testaa a condicao depois de executar o bloco.
+//          -> dessa forma sera exutado ao menos 1x
+
+
+//int idade = 1;
+
+//do
+//{
+//    Console.WriteLine($"Repeticao do numero : {idade}");
+//    //Console.WriteLine("Repeticao do numero : " + idade);
+//    // codigo sera executado ao menos uma vez.
+
+
+//} while (idade > 5); // condicao.
+
+
+
+// O usuario deve enviar a senha corretamente.Enquanto ele errar Solicite para enviar a senha novamente . A senha deve ser Senai134.
+
+//Console.WriteLine("Digite sua senha : ");
+//string senha = Console.ReadLine();
+
+//while (senha != "Senai134")
+//{
+//    Console.WriteLine("Senha Incorreta");
+//    Console.WriteLine("Digite a Senha Novamente : ");
+//    senha = Console.ReadLine();
+//}
+
+////---------------------------------------------------------------
+
+//do
+//{
+//    Console.WriteLine("Senha Incorreta");
+//    Console.WriteLine("Digite a Senha novamente : ");
+//    senha = Console.ReadLine();
+
+//} while (senha != "Senai134"); 
+
+
